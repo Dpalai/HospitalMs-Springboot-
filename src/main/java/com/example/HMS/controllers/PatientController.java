4453e130d025baea45e85ccc3f92f7ef4caab9cf
miku <palaimiku04@gmail.com>
2026-09-26 14:01:04 +0530

    feat: Add pagination and switch logging framework to Log4j2

--- src/main/java/com/example/HMS/controllers/PatientController.java
+++ src/main/java/com/example/HMS/controllers/PatientController.java
@@ -3,13 +3,17 @@ package com.example.HMS.controllers;
 
 import com.example.HMS.model.Patient;
 import com.example.HMS.service.PatientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/V1/Patients")
+@Slf4j
 public class PatientController {
 
     // connecting with the service layer
@@ -25,9 +29,22 @@ public class PatientController {
     }
 
     @GetMapping
-    public List<Patient> getAllPatient(){
-        //System.out.println("all Patients");
-        return patientService.getAllPatient();
+    // public List<Patient> getAllPatient(){
+
+    // for Paggination @RequestParam....      //   we can remove default values but we
+                                              // need to type the url for page and size,
+                                              //  ** if we don't want to write that url then we
+                                              // must explicitly set required = false and -->
+                                              // @RequestParam(required = false) Integer page,
+                                              // @RequestParam(required = false) Integer size
+                                              // we must switch the parameter types to the object wrapper Integer
+    public Page<Patient> getAllPatient(@RequestParam(defaultValue = "0") int page,
+                                       @RequestParam(defaultValue = "5") int size) {
+      // ** // int pageNumber = (page != null) ? page : 0;
+            // int pageSize = (size != null) ? size : 10;
+        System.out.println("all Patients");
+        log.info("Controller : GEt all Patients");
+        return patientService.getAllPatient(page,size);
     }
 
 
--- src/main/java/com/example/HMS/service/PatientService.java
+++ src/main/java/com/example/HMS/service/PatientService.java
@@ -2,15 +2,28 @@ package com.example.HMS.service;
 
 import com.example.HMS.model.Patient;
 import com.example.HMS.repository.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
+// import java.util.logging.Logger;
 
 
 @Service
+@Slf4j   // Lombok annnotation to use the Log4j2 looger
 public class PatientService {
+
+    // instade of the bellow line we can also use the annotation @Slf4j ,beacuse we use Lombok in this project.
+
+   //private static final Logger logger= LoggerFactory.getLogger(PatientService.class);
 
     @Autowired
     private PatientRepository patientRepository;
@@ -18,10 +31,23 @@ public class PatientService {
 
     public Patient CreatePatient(Patient patient){
         try {
-            //System.out.println("create pateint service");
+            // console output
+             System.out.println("into pateint service layer");
+             // when we use Slf4j --> Log4J2
+             log.info("service:into Create Pateint Service Layer");
             return patientRepository.save(patient);
             //return  patient;
         }catch (Exception e){
+            // consol output
+            System.out.println("Error message:" + e.getMessage());
+                 //logger msg  if we use the object of Logger
+
+           //  logger.error("An Error Occured While Creating Patients: {}",e.getMessage()  );
+
+                // if we use Lombok annotation
+            log.error("An Error Occured While Creating Patients: {}",e.getMessage()  );
+
+
             e.printStackTrace();
             return null;
         }
@@ -29,13 +55,18 @@ public class PatientService {
     }
 
 
-    public List<Patient> getAllPatient(){
+    // public List<Patient> getAllPatient(){
+    // paggiination
+    public Page<Patient> getAllPatient(int page, int size){
         try {
             //System.out.println("gell patient service");
-            return patientRepository.findAll();
+            log.info("service:into Get All Pateint Service Layer");
+            Pageable pageable = PageRequest.of(page, size);
+            return patientRepository.findAll(pageable);
         }catch (Exception e){
             System.out.println("error:"+ e.getMessage());
-            return null;
+            log.error("An Error Occured While Fetching All Patients: {}",e.getMessage()  );
+            return Page.empty();
         }
     }
 
@@ -52,10 +83,12 @@ public class PatientService {
 
             */
             // (OR we can Do)
+            log.info("service:into GET Patient By Id Service Layer");
             return patientRepository.findById(id).orElse(null);
 
         }catch (Exception e){
-            e.printStackTrace();
+            System.out.println("error:"+ e.getMessage());
+            log.error("An Error Occured While Getting Patient by id {} Patients: {}",id,e.getMessage()  );
             return null;
         }
 
@@ -65,8 +98,11 @@ public class PatientService {
         try {
            // System.out.println("getpatientService");
             patientRepository.deleteById(id);
+            log.info("service:into Delet Patient By Id Pateint Service Layer");
 
         }catch (Exception e){
+            System.out.println("error:"+ e.getMessage());
+            log.error("An Error Occured While Deleting  Patient bY id {} : {}",id,e.getMessage()  );
             e.printStackTrace();
         }
     }
@@ -82,9 +118,13 @@ public class PatientService {
                 p.setGender(upadetpatient.getGender());
                 patientRepository.save(p);
 
+                log.info("service:into Update Patient By Id Pateint Service Layer");
+
             }
 
         }catch (Exception e){
+            System.out.println("error:"+ e.getMessage());
+            log.error("An Error Occured While Updating  Patient by id {} : {}",id,e.getMessage()  );
             e.printStackTrace();
 
         }
